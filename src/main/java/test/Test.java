50127913dcde0bf5b7d56cc37dc4545aa47104af
fudada123 <2210384480@qq.com>
2019-05-13 19:55:57 +0800

    666

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author fudada
+ * @date 2019/5/13 - 19:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("6666");
+    }
+}
